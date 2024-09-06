5de408f29a546bb59ac1529a727ac00cb0ad0f0f
Yazid Habibi <jusuf1909@student.abudzar.sch.id>
2024-09-06 13:41:23 +0700

    s

---
+++ meeting12/latihanskilvull.js
@@ -0,0 +1,16 @@
+/*function mandi() {
+  console.log("mandi");
+}
+
+function sarapan(callback) {
+  console.log("Sarapan tertunda 3 detik");
+  setTimeout(callback, 3000);
+}
+
+function berangkatSekolah() {
+  console.log("berangkat sekolah");
+}
+
+mandi();
+sarapan(berangkatSekolah); */
+
