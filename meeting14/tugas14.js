d2c069870217ec8a699ca274056fc5cc23ef7f80
Yazid Habibi <jusuf1909@student.abudzar.sch.id>
2024-09-20 11:30:21 +0700

    belom kelar

--- meeting14/tugas14.js
+++ meeting14/tugas14.js
@@ -1,33 +1,41 @@
-
 //soal 1 start
 
-function simulateDelay() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Delay Finished");
-    }, 2000);
-  });
-}
+// function simulateDelay() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("Delay Finished");
+//     }, 2000);
+//   });
+// }
 
-async function result() {
-  let rst = await simulateDelay();
-  console.log(rst);
-}
+// async function result() {
+//   let rst = await simulateDelay();
+//   console.log(rst);
+// }
 
-console.log(result());
+// console.log(result());
 
 //soal 1 clear
 
 //soal 2 start
 
-async function calculateDivision(a, b){
-  let div = a / b;
-  if (b == 0 || a == 0){
-    throw new Error("error");{
+async function calculateDivision(angka1, angka2) {
+  return await new Promise(() => {
+    try {
+      if (angka2 === 0) {
+        throw new Error("Pembagi tidak boleh 0");
+      }
+      const hasil = angka1 / angka2;
 
+      console.log(hasil);
+    } catch (error) {
+      console.log(error.message);
     }
-  }
+  });
 }
 
+calculateDivision(12, 0);
 
+//soal 2 clear
 
+//soal 3 start
